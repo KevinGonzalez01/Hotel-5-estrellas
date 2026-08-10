e215ab063af410dd1cd7b051a4be922a1c08cf91
KevinGonzalez01 <kgonzalezb500@inemkennedy.edu.co>
2026-08-09 23:37:02 -0500

    Inicio hecho

--- Js/script.js
+++ Js/script.js
@@ -3,15 +3,56 @@ const images = document.querySelectorAll(".slides img");
 
 let index = 0;
 
-function cambiarImagen(){
+function cambiarImagen() {
+
     index++;
 
-    if(index >= images.length){
+    if (index >= images.length) {
         index = 0;
     }
 
     slides.style.transform = `translateX(-${index * 100}%)`;
-    slides.style.transition = "0.8s";
 }
 
-setInterval(cambiarImagen, 3000);
+setInterval(cambiarImagen, 4000);
+
+
+
+/**Habitaciones*/
+
+const habitaciones = [
+    {
+        titulo: "Habitación estándar",
+        descripcion: "Una habitación cómoda y acogedora para una estadía agradable.",
+        precio: "$120.000"
+    },
+    {
+        titulo: "Habitación deluxe",
+        descripcion: "Una habitación más amplia con mayor comodidad y excelentes servicios.",
+        precio: "$180.000"
+    },
+    {
+        titulo: "Suite premium",
+        descripcion: "Una suite amplia y elegante, perfecta para una estadía especial.",
+        precio: "$250.000"
+    },
+    {
+        titulo: "V.I.P",
+        descripcion: "Una habitacion con todos los lujos, amplia y elegante.",
+        precio: "$350.000"
+    }
+];
+
+function cambiarInfo(numero) {
+    document.getElementById("titulo").textContent =
+        habitaciones[numero].titulo;
+
+    document.getElementById("descripcion").textContent =
+        habitaciones[numero].descripcion;
+
+    document.getElementById("precio").textContent =
+        habitaciones[numero].precio;
+
+    document.getElementById("precio").textContent =
+        habitaciones[numero].precio;
+}
